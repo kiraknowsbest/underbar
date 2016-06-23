3708af529ed435b5c0746f8e5bbe34dbf5e0d00e
Daniel Metzger <kinaharo@gmail.com>
2016-06-22 22:06:59 -0700

    Pass _.uniq

--- src/underbar.js
+++ src/underbar.js
@@ -99,6 +99,13 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var results = [];
+
+    _.each( array, function( item ){
+      _.indexOf( results, item ) === -1 && ( results.push( item ));
+    });
+
+    return results;
   };
 
 
